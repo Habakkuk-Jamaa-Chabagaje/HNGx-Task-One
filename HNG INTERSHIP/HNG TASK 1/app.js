730eee84bc7482b5a6a8ffaa8073129e222e09f4
Jamaa Habakkuk C <122156094+Habakkuk-Jamaa-Chabagaje@users.noreply.github.com>
2023-09-09 03:54:51 -0700

    Add files via upload
    HNGx Task One created by me which contains my image, name, stack, real time and day as instructed on the task script.

---
+++ HNG INTERSHIP/HNG TASK 1/app.js
@@ -0,0 +1,52 @@
+// javaScript section for the required time and date functionalities
+
+let dayEl = document.getElementById("currentDay");
+let timeEl = document.getElementById("currentTime");
+
+const daysArray = [
+    "Sunday", 
+    "Monday", 
+    "Tuesday", 
+    "Wednesday", 
+    "Thursday", 
+    "Friday", 
+    "Saturday"
+];
+
+
+function timeView(){
+    let range = 1000;
+    let rangeTime = setTimeout('displayTime()',range);
+}
+
+function displayTime() {
+    const live = new Date();
+    let toDay = "Day: " + daysArray[live.getDay() ] ;
+    dayEl.innerHTML = toDay;
+
+    const timer = new Date();
+    const seconds = timer.getUTCMilliseconds();
+    const minutes = timer.getUTCMinutes();
+    const hour = timer.getUTCHours();
+
+    const timeNow = ` Time ${hour}:${minutes}:${seconds}`;
+    timeEl.innerHTML = timeNow;
+    timeView();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
